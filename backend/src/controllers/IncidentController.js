76dbc56733ecb037c362a0ed2c39931a515f3502
Rafael Sandim <rjsandim@gmail.com>
2020-03-29 01:38:13 -0300

    Front-end finalizado

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -25,11 +25,15 @@ module.exports = {
         const { title, description, value } = request.body;
         const ong_id = request.headers.authorization;
 
-        const [id] = await connection('incidents').insert({
-            title, description, value, ong_id
-        });
+        try {
+            const [id] = await connection('incidents').insert({
+                title, description, value, ong_id
+            });
 
-        return response.json({ id });
+            return response.json({ id });
+        } catch (err) {
+            return response.json({ err });
+        }
     },
     async delete(request, response) {
         const { id } = request.params;
